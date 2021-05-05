f7edf3913d6074b366a56fc0575d81036febf342
ThunderStruck71 <iovodovi@yandex.ru>
2021-05-05 19:12:57 +0300

    build(app): add start frontend configuration

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { hot } from "react-hot-loader/root";
+
+export const App = hot(_App);
+function _App(): JSX.Element | null {
+    return (
+        <div>
+            <h1>Hello Chat-Chat!</h1>
+        </div> 
+    );
+};
---
+++ frontend/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path")
+const webpack = require("webpack")
+
+module.exports = {
+    entry: "./src/index.tsx", // точка входа, о которой говорилось ранее.
+    mode: "development",
+    module: {
+        rules: [
+            {
+                test: /\.[jt]sx?$/, // сопоставляет файлы .js, .ts, и .tsx 
+                loader: "babel-loader", // использует для указанных типов файлов загрузчик babel-loader (ts-loader не требуется).
+                exclude: /node_modules/, 
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"], 
+            },
+        ],
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"],
+    },
+    output: {
+        filename: "bundle.js",
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "public/"),
+        port: 3003,
+        publicPath: "http://localhost:3003/dist/",
+        hotOnly: true,
+    },
+    plugins: [new webpack.HotModuleReplacementPlugin()],
+    devtool: "eval-source-map",
+}
