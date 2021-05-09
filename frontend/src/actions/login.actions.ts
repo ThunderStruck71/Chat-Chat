4567de81ad96bfdbb57a37d98937db4ab10f404d
ThunderStruck71 <iovodovi@yandex.ru>
2021-05-09 17:47:07 +0300

    feat(src): add actions

---
+++ frontend/src/actions/login.actions.ts
@@ -0,0 +1,58 @@
+import { LOGIN_FAILURE, LOGIN_REQUEST, LOGIN_SUCCESS, LOGOUT } from "../contants";
+import { Dispatch } from "../helpers";
+import { loginServices } from "../services";
+import { User } from "../types";
+
+type LoginRequestAction = {
+    type: typeof LOGIN_REQUEST
+};
+
+type LoginSuccessAction = {
+    type: typeof LOGIN_SUCCESS,
+    payload: User
+};
+
+type LoginFailureAction = {
+    type: typeof LOGIN_FAILURE,
+    reason: Error
+};
+
+type LogoutAction = {
+    type: typeof LOGOUT
+};
+
+function login(userName: string) {
+    return (dispatch: Dispatch) => {
+        dispatch({
+            type: LOGIN_REQUEST
+        });
+
+        return loginServices.signIn(userName)
+            .then(user => {
+                dispatch({
+                    type: LOGIN_SUCCESS,
+                    payload: user
+                })
+            })
+            .catch(error => {
+                dispatch({
+                    type: LOGIN_FAILURE,
+                    reason: error
+                })
+            });
+    }
+};
+
+function logout(): LogoutAction {
+    return {
+        type: LOGOUT
+    };
+};
+
+export const loginActions = {
+    login,
+    logout
+}
+
+export type LoginAction = LoginRequestAction | LoginSuccessAction | LoginFailureAction | LogoutAction;
+
