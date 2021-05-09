edb7989ff3777f40be5617283966cbf8aa42356e
ThunderStruck71 <iovodovi@yandex.ru>
2021-05-09 18:53:52 +0300

    refactor(src): update history

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,18 @@
 
+import { history } from "./helpers";
 import React from "react";
 import { hot } from "react-hot-loader/root";
-import { Redirect, Route, Switch } from "react-router";
+import { Redirect, Route, Router, Switch } from "react-router";
 import { LoginPage } from "./pages";
 
 function _App(): JSX.Element | null {
     return (
-        <Switch>
-            <Route path="/login" component={LoginPage}/>
-            <Redirect from="/" to="/login"/>
-        </Switch>
+        <Router history={history}>
+            <Switch>
+                <Route path="/login" component={LoginPage} />
+                <Redirect from="*" to="/login" />
+            </Switch>
+        </Router>
     );
 };
 
