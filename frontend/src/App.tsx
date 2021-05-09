300ef85ad5eb27ad062f7d31fa0ed9a683257da4
ThunderStruck71 <iovodovi@yandex.ru>
2021-05-09 18:58:23 +0300

    feat(src): add new route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,12 +3,14 @@ import { history } from "./helpers";
 import React from "react";
 import { hot } from "react-hot-loader/root";
 import { Redirect, Route, Router, Switch } from "react-router";
-import { LoginPage } from "./pages";
+import { ChatRoomPage, LoginPage } from "./pages";
+import { PrivateRoute } from "./components";
 
 function _App(): JSX.Element | null {
     return (
         <Router history={history}>
             <Switch>
+                <PrivateRoute path="/chatRoom" component={ChatRoomPage} />
                 <Route path="/login" component={LoginPage} />
                 <Redirect from="*" to="/login" />
             </Switch>
