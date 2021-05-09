d397502bf8421b08b83e4238d938535d06f6e30f
ThunderStruck71 <iovodovi@yandex.ru>
2021-05-09 17:55:40 +0300

    refactor(src): update store

--- frontend/src/store.ts
+++
@@ -1,8 +0,0 @@
-import rootReducer from "./reducers/rootReducer";
-import { createStore } from "redux";
-
-const store = createStore(rootReducer);
-
-export type Dispatch = typeof store.dispatch;
-
-export default store;
