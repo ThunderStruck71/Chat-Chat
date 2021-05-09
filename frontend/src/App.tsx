97343822f03758b4a7b1a449b5daf6e824474028
ThunderStruck71 <iovodovi@yandex.ru>
2021-05-09 17:50:29 +0300

    refactor(src): update app component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 import { hot } from "react-hot-loader/root";
 import { Redirect, Route, Switch } from "react-router";
-import {LoginPage} from "./pages";
+import { LoginPage } from "./pages";
 
 function _App(): JSX.Element | null {
     return (
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import {BrowserRouter as Router} from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import { App } from "./App";
-import store from "./store";
+import { store } from "./helpers";
 
 ReactDOM.render((
     <React.StrictMode>
