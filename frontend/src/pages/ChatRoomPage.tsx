81ad43057d71f757fdd2a24cbf6ae114a9c335b9
ThunderStruck71 <iovodovi@yandex.ru>
2021-05-10 15:24:17 +0300

    feat(src): change component

--- frontend/src/pages/ChatRoomPage.tsx
+++ frontend/src/pages/ChatRoomPage.tsx
@@ -1,9 +1,21 @@
+import { loginActions } from "../actions";
 import React, { FC } from "react"
+import { useSelector } from "react-redux";
+import { RootState } from "../reducers";
 
 export const ChatRoomPage: FC = () => {
+    const { user } = useSelector((state: RootState) => state.login);
+
+    const handleClick = () => {
+        loginActions.logout();
+    }
+
     return (
         <div>
-            Chat-Chat
+            Welcome to Chat-Chat, {user?.userName}
+            <div>
+                <button onClick={handleClick}>SignOut</button>
+            </div>
         </div>
     );
 };
