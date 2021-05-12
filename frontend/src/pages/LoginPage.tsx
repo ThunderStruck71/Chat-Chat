2fbd019bb5870709bde52d491bd91804371f00d0
iOvodov <iovodov@rfibank.ru>
2021-05-12 19:02:34 +0300

    refactor(src): rename state field

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,17 +1,17 @@
 import { loginActions } from "../actions";
 import React, { FC, useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch} from "react-redux";
 
 export const LoginPage: FC = () => {
     const [inputValue, setInputValue] = useState("");
-    const [error, setError] = useState(false);
+    const [fieldNotEmpty, setFieldNotEmpty] = useState(false);
     const dispatch = useDispatch();
 
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setInputValue(event.target.value);
 
-        if (error) {
-            setError(false);
+        if (fieldNotEmpty) {
+            setFieldNotEmpty(false);
         }
     };
 
@@ -19,7 +19,7 @@ export const LoginPage: FC = () => {
         event.preventDefault();
 
         if (!inputValue) {
-            setError(true);
+            setFieldNotEmpty(true);
             return;
         }
 
@@ -32,7 +32,7 @@ export const LoginPage: FC = () => {
             <h2>Введите имя</h2>
             <input type="text" placeholder="Имя пользователя чата" onChange={handleChange} value={inputValue}/>
             <button>Войти в чат</button>
-            {error && <div>Поле не заполнено</div>}
+            {fieldNotEmpty && <div>Поле не заполнено</div>}
         </form>
     );
 };
