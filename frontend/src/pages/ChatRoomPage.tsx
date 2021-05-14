e67a53f9d27d78a69bb994d1d5c888ed816ccaf4
ThunderStruck71 <iovodovi@yandex.ru>
2021-05-14 14:12:39 +0300

    feat(src): update chat page

--- frontend/src/pages/ChatRoomPage.tsx
+++ frontend/src/pages/ChatRoomPage.tsx
@@ -1,22 +1,20 @@
-import { loginActions } from "../actions";
-import React, { FC } from "react"
+import { loginActions, usersActions } from "../actions";
+import React, { FC, useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../reducers";
 
 export const ChatRoomPage: FC = () => {
     const { user } = useSelector((state: RootState) => state.login);
-
+    const { users } = useSelector((state: RootState) => state.users);
     const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(usersActions.getAll());
+    }, [])
 
     const handleClick = () => {
         dispatch(loginActions.logout());
     }
-
-    const users = [
-        { id: 0, name: "Ivan" },
-        { id: 1, name: "Dima" },
-        { id: 2, name: "Ilya" },
-    ];
 
     return (
         <div>
@@ -28,10 +26,10 @@ export const ChatRoomPage: FC = () => {
                 <div style={{display: "flex"}}>
                     <div style={{ display: "flex", width: "50%", height: "300px", border: "1px solid black", borderRadius: "8px", margin: "5px 0" }} />
 
-                    <div style={{ display: "flex", alignItems: "center", flexDirection: "column", width: "50%" }}>
+                    <div style={{ display: "flex", alignItems: "center", flexDirection: "column", width: "50%", height: "300px" }}>
                         <h3>Список пользователей</h3>
-                        <ul>
-                            {users.map(user => (<li key={user.id}>{user.name}</li>))}
+                        <ul style={{overflow: "auto", width: "inherit", listStyleType: "none"}}>
+                            {users.map((user, index) => (<li key={index}>{user.userName}</li>))}
                         </ul>
                     </div>
                 </div>
